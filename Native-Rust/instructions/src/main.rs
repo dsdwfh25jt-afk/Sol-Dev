2404beebf3ffa9fc9c8d4014649544a610c92b46
Gaurav <gauravbhat7121@icloud.com>
2026-04-09 19:44:34 +0530

    added airdrop function and dployed live on devnet

--- Native-Rust/instructions/src/main.rs
+++ Native-Rust/instructions/src/main.rs
@@ -5,12 +5,36 @@ use solana_sdk::{
     signer::keypair::Keypair
 };
 use solana_system_interface::instruction::transfer;
+use solana_client::nonblocking::rpc_client::RpcClient;
+use solana_commitment_config::CommitmentConfig;
+use std::io;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     
     let sender = Keypair::new();
     let reciever = Keypair::new();
+
+    let connection = RpcClient::new_with_commitment(
+        "https://api.devnet.solana.com".to_string(),
+        CommitmentConfig::confirmed(),
+    ); 
+
+    // let wallet = Keypair::new();
+    println!("Enter the amount of SOL tokens : ");
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)?;
+     
+    let sol : f64 = input.trim().parse().expect("Invalid number ");
+    let lamps = (sol * LAMPORTS_PER_SOL as f64) as u64;
+
+    let airdrop_signature = connection
+        .request_airdrop(&sender.pubkey(),lamps)
+        .await?;
+
+    connection.poll_for_signature_confirmation(&airdrop_signature, 1).await?;
+
 
     let transfer_amount = LAMPORTS_PER_SOL / 100;
 
@@ -20,7 +44,7 @@ async fn main() -> Result<()> {
         transfer_amount,
     );
 
-    println!("{:?}", transfer_instuction);
+    println!("{:#?}", transfer_instuction);
 
     Ok(())
 }

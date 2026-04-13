1494c561858f5631b5f45f91325e406c02ae79a3
Gaurav <gauravbhat7121@icloud.com>
2026-04-13 23:00:24 +0530

    added mutiple seeds for pda

--- Native-Rust/PDA-Derivation/src/main.rs
+++ Native-Rust/PDA-Derivation/src/main.rs
@@ -6,8 +6,8 @@ async fn main() -> anyhow::Result<()> {
     let program_address = Pubkey::from_str("11111111111111111111111111111111")?;
 
     let optional_seed_add = Pubkey::from_str("B9Lf9z5BfNPT4d5KMeaBFx8x1G4CULZYR1jA2kmxRDkA")?;
-    // let seeds: &[&[u8]] = &[b"helloWorld"];
-    let seeds : &[&[u8]] = &[optional_seed_add.as_ref()];
+    let byte_seeds = b"helloWorld";
+    let seeds : &[&[u8]] = &[byte_seeds, optional_seed_add.as_ref()];
 
     let (pda, bump) = Pubkey::find_program_address(seeds, &program_address);
 

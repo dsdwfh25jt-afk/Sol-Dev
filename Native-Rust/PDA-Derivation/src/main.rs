9484ebaea0d8ca81b0d88ca35598e915d8ab7d7d
Gaurav <gauravbhat7121@icloud.com>
2026-04-13 23:08:37 +0530

    added suitable comments

--- Native-Rust/PDA-Derivation/src/main.rs
+++ Native-Rust/PDA-Derivation/src/main.rs
@@ -3,10 +3,17 @@ use std::str::FromStr;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    // owner of pda = program address 
     let program_address = Pubkey::from_str("11111111111111111111111111111111")?;
 
+    // address seed uses for differentiate mutiple account's  pda 
     let optional_seed_add = Pubkey::from_str("B9Lf9z5BfNPT4d5KMeaBFx8x1G4CULZYR1jA2kmxRDkA")?;
+
+    // another seed (string must in byte form)
     let byte_seeds = b"helloWorld";
+    // let seed : &[&[u8]] = &[b"helloWorld"];  for only just single string seed 
+
+    // generates seed from mutiple seed , actually its just concatination (byte_seed + optional_seed_add)
     let seeds : &[&[u8]] = &[byte_seeds, optional_seed_add.as_ref()];
 
     let (pda, bump) = Pubkey::find_program_address(seeds, &program_address);

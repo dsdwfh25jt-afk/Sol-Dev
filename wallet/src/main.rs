1561612da7fbc158abac9553c22c0346442aad98
Gaurav <gauravbhat7121@icloud.com>
2026-04-16 19:18:09 +0530

    added function to import account from seed

--- wallet/src/main.rs
+++ wallet/src/main.rs
@@ -6,18 +6,20 @@
 use std::fs;
 use std::path::PathBuf;
 use std::path::Path;
-use solana_sdk::signer::{keypair::Keypair, Signer};
-// use anyhow::Result;
-// use bip39::{Language, Mnemonic, Seed};
+use solana_sdk::signer::{keypair::{Keypair, keypair_from_seed}, Signer };
+use anyhow::Result;
+use bip39::{Language, Mnemonic};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()>{
 
     // creating new keypair / account 
     let keypair = Keypair::new();
     println!("Public Key: {}", keypair.pubkey());
     println!("Secret Key: {:?}", keypair.to_bytes());
     save_key_to_project();
+    import_from_seed();
+     Ok(())
 }
 
 
@@ -40,6 +42,45 @@ fn save_key_to_project() {
         Err(e) => println!("Failed to save the key: {}", e),
     }
 
-
-
 }
+
+//  importing wallet from mnenommics 
+    fn import_from_seed() {
+    let phrase = "pill tomorrow foster begin walnut borrow virtual kick shift mutual shoe scatter";
+    let mnemonic = Mnemonic::parse_in(Language::English, phrase).expect("Invalid mnemonic phrase entered!");
+
+    let seed = mnemonic.to_seed("");
+    let keypair = keypair_from_seed(seed.as_ref()).unwrap();
+
+    println!("recovered address {:?}", keypair.pubkey());
+    }
+
+
+
+// use std::io::{self, Write};
+// // Assuming you have your import_wallet_from_mnemonic function from earlier
+
+// fn main() {
+//     // 1. Print the prompt. We use print! instead of println! so the input 
+//     // cursor stays on the same line.
+//     print!("Please enter your 12 or 24-word mnemonic phrase: ");
+    
+//     // 2. We must flush stdout to ensure the print! macro displays immediately
+//     io::stdout().flush().expect("Failed to flush stdout");
+
+//     // 3. Create a mutable string to hold the user's input
+//     let mut input = String::new();
+
+//     // 4. Read the line from the terminal
+//     io::stdin()
+//         .read_line(&mut input)
+//         .expect("Failed to read line");
+
+//     // 5. Trim the invisible newline characters (\n or \r\n) from the end
+//     let phrase = input.trim();
+
+//     println!("You entered: {}", phrase); // Just for testing! Remove in production.
+    
+//     // 6. Pass the sanitized phrase to your existing function
+//     // import_wallet_from_mnemonic(phrase);
+// }

86f81016320e7ca5a2c50bc950c0777ef3e220a6
dsdwfh25jt-afk <gauravbhat7121@icloud.com>
2026-04-29 15:43:03 +0000

    created vote for car and initiated with 0 all the votes

---
+++ Anchor-/vot-for-car/programs/vot-for-car/src/lib.rs
@@ -0,0 +1,36 @@
+use anchor_lang::prelude::*;
+
+declare_id!("FHXXfUX2UVCpVWn2iTTYGA23fnUCyUAXpWwARJhJFTuJ");
+
+#[program] 
+
+pub mod vot_for_car {
+    use super::*;
+    pub fn initial(ctx: Context<User>) -> Result<()> {
+        let vot_of_cars = &mut ctx.accounts.cars;
+        vot_of_cars.ferrari = 0;
+        vot_of_cars.mclarren = 0;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct User <'info>{
+    #[account(mut)]
+    signer : Signer<'info>,
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + 16 ,
+        
+    )]
+    cars : Account <'info , Cars>,
+    system_program : Program<'info, System>,
+}
+
+
+#[account] 
+pub struct Cars {
+    pub ferrari : u64,
+    pub mclarren : u64,
+}
